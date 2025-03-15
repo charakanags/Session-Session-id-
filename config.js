00e384d92921f713f8a746940436c1165d219a1a
charakanags <repomax074@gmail.com>
2025-03-15 16:05:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "MINUKI-MD=EGdEwL4b#Ma32EjHFYS_E0uCqxvk6vJcJVC36K8lAlctDfE6V-Yo",
+SESSION_ID: process.env.SESSION_ID || "c9IlyaSZ#yt_neXYktpx4XrKk23t-0AbgTntSGMRxArWxjTTQCcU",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -15,58 +15,58 @@ AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY QUEEN-MINUKI-MD 🤍*",
 // set the auto reply massage on status reply  
-PREFIX: process.env.PREFIX || "/",
+PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "QUEEN-MINUKI-MD",
+BOT_NAME: process.env.BOT_NAME || "𝕮𝕳𝕸𝕴𝕹𝕯𝖀",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "QUEEN-MINUKI-MD",
+STICKER_NAME: process.env.STICKER_NAME || "𝕮𝕳𝕬𝕸𝕴𝕹𝕯𝖀",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,🫟,🫟,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
+DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94741354157",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94783314361",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Ａᴅʜɪ ᴏꜰᴄ",
+OWNER_NAME: process.env.OWNER_NAME || "𝘾𝙃𝘼𝙈𝙄𝙉𝘿𝙐",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀᴅʜɪ ᴏꜰᴄ*",
+DESCRIPTION: process.env.DESCRIPTION || "*© 𝙃𝙄 𝙄𝘼𝙈 𝙒𝘼 𝘽𝙊𝙏*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://pomf2.lain.la/f/32igu48i.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> `QUEEN-MINUKI-MD BOT IS ALIVE NOW` *MINUKI-MD*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> 𝘾𝙃𝙈𝙄𝙉𝘿𝙐 BOT IS ALIVE NOW` *𝘾𝙃𝙈𝙄𝙉𝘿𝙐-𝙈𝘿*⚡",
 // add alive msg here 
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
+READ_MESSAGE: process.env.READ_MESSAGE || "true",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
+ANTI_BAD: process.env.ANTI_BAD || "true",
 // false or true for anti bad words  
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
-ANTI_LINK: process.env.ANTI_LINK || "false",
+ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
 // make true for send automatic voices
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
 // make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 // make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
 // true for automatic show typing   
-READ_CMD: process.env.READ_CMD || "false",
+READ_CMD: process.env.READ_CMD || "true",
 // true if want mark commands as read 
-DEV: process.env.DEV || "94756310995",
+DEV: process.env.DEV || "94783314361",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
 // change it to 'same' if you want to resend deleted message in same chat 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 // make it true for auto recoding 
 };
