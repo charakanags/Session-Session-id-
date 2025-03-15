9467faa33f28626e276dc54c5d92a4c2d450760b
charakanags <repomax074@gmail.com>
2025-03-15 16:24:08 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,12 +13,12 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         // Generate system status message
-        const status = `╭━━〔 *CHAMA-MD* 〕━━┈⊷
+        const status = `╭━━〔 *𝘾𝙃𝘼𝙈𝙄𝙉𝘿𝙐-MD* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *⏳Uptime*:  ${runtime(process.uptime())} 
 ┃◈┃• *📟 Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
 ┃◈┃• *⚙️ HostName*: ${os.hostname()}
-┃◈┃• *👨‍💻 Owner*: Ａᴅʜɪ ᴏꜰᴄ
+┃◈┃• *👨‍💻 Owner*: 𝘾𝙃𝘼𝙈𝘼
 ┃◈┃• *🧬 Version*: 1.0.0 BETA
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
@@ -27,15 +27,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
-            image: { url: `https://files.catbox.moe/ospihv.jpg` },  // Image URL
+            image: { url: `...................𝙝𝙞.img` },  // Image URL
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363292101892024@newsletter',
-                    newsletterName: 'ᴀᴅʜɪ ᴏꜰᴄ',
+                    newsletterJid: '0029Vb9WF4nJJhzeUCFS6M0u@channel',
+                    newsletterName: '💜💜💜💜',
                     serverMessageId: 143
                 }
             }
